62fc6eb56691076294cb4e45d73a81916616278a
marlontoe <inclusionrec@googlemail.com>
2014-12-04 15:03:35 +0100

    imported lionheart cpu governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -393,6 +393,9 @@ extern struct cpufreq_governor cpufreq_gov_adaptive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
 extern struct cpufreq_governor cpufreq_gov_lazy;
 #define CPUFREQ_DEFAULT_GOVERNOR      (&cpufreq_gov_lazy)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR      (&cpufreq_gov_lionheart)
 #endif
 
 
