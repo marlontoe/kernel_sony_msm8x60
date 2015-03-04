895586c7a79c806ddb4f8a5d3eab6df83f788e56
cb <billchen1977@gmail.com>
2015-03-04 13:05:40 +0800

    cpufreq: smartmax: fix rwsem deadloak when policy start on suspend

--- drivers/cpufreq/cpufreq_smartmax.c
+++ drivers/cpufreq/cpufreq_smartmax.c
@@ -1296,9 +1296,7 @@ static int cpufreq_governor_smartmax(struct cpufreq_policy *new_policy,
 				mutex_unlock(&dbs_mutex);
 				return rc;
 			}
-#ifdef CONFIG_HAS_EARLYSUSPEND
-			register_early_suspend(&smartmax_early_suspend_handler);
-#endif
+
 			/* policy latency is in nS. Convert it to uS first */
 			latency = new_policy->cpuinfo.transition_latency / 1000;
 			if (latency == 0)
@@ -1346,9 +1344,6 @@ static int cpufreq_governor_smartmax(struct cpufreq_policy *new_policy,
 			input_unregister_mediator_secondary(&smartmax_input_mediator_handler);
 #else
 			input_unregister_handler(&dbs_input_handler);
-#endif
-#ifdef CONFIG_HAS_EARLYSUSPEND
-			unregister_early_suspend(&smartmax_early_suspend_handler);
 #endif
 		}
 		
@@ -1417,6 +1412,7 @@ static int __init cpufreq_smartmax_init(void) {
 	smartmax_early_suspend_handler.suspend = smartmax_early_suspend;
 	smartmax_early_suspend_handler.resume = smartmax_late_resume;
 	smartmax_early_suspend_handler.level = EARLY_SUSPEND_LEVEL_DISABLE_FB + 100;
+	register_early_suspend(&smartmax_early_suspend_handler);
 #endif
 	
 	return cpufreq_register_governor(&cpufreq_gov_smartmax);
@@ -1431,6 +1427,10 @@ module_init(cpufreq_smartmax_init);
 static void __exit cpufreq_smartmax_exit(void) {
 	unsigned int i;
 	struct smartmax_info_s *this_smartmax;
+
+#ifdef CONFIG_HAS_EARLYSUSPEND
+	unregister_early_suspend(&smartmax_early_suspend_handler);
+#endif
 
 	cpufreq_unregister_governor(&cpufreq_gov_smartmax);
 
